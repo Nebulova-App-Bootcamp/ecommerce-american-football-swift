0db62c0b01fdb4204eac99169437e56acc31f4e1
jose09511 <jose09511@gmail.com>
2021-11-12 20:29:22 +0100

    init project

---
+++ ecommerce-american-football-swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ecommerce-american-football-swift
+//
+//  Created by Jose Manuel Barba Sánchez on 12/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ecommerce-american-football-swift/ecommerce_american_football_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecommerce_american_football_swiftApp.swift
+//  ecommerce-american-football-swift
+//
+//  Created by Jose Manuel Barba Sánchez on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ecommerce_american_football_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
